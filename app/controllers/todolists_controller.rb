49b33a67cd642b85c2762ad65fe4f6638dd96082
besumero628 <besumero628@gmail.com>
2021-06-28 07:10:52 +0000

    [Update] 2-6 update action

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -18,6 +18,10 @@ class TodolistsController < ApplicationController
     redirect_to todolist_path(list.id)
   end
   
+  def update
+    
+  end
+  
   private
   def list_params
     params.require(:list).permit(:title, :body)
