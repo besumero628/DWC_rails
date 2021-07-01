97d4fed9adb41e23132594a66e571d85dcf95de7
besumero628 <besumero628@gmail.com>
2021-07-01 05:54:43 +0000

    [Finish] 2-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '7625d0cf7a8c52b97a7cd75b5523ae341cc182fb24265e30fb207e0cd4851cf9a9d1c24fc3e85523649c0eb29ecc9c0ca007e8022f21c99e6ff845e375d5da5b'
